06c77e995fa0969082b12d19ade252b3a9874c48
LaMalintzin <34371323+MartinGyllstrom@users.noreply.github.com>
2024-03-22 20:07:32 +0100

    Finished SplashActivity

--- app/src/main/java/com/example/actionplannerapplication/SplashActivity.java
+++ app/src/main/java/com/example/actionplannerapplication/SplashActivity.java
@@ -1,13 +1,16 @@
 package com.example.actionplannerapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.os.Handler;
 public class SplashActivity extends AppCompatActivity {
 
     @Override
@@ -15,6 +18,19 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 4000);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
