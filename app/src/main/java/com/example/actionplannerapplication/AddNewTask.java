275a5ed521cfe4e00512f3c1a4efbc2608a12641
LaMalintzin <34371323+MartinGyllstrom@users.noreply.github.com>
2024-03-27 20:11:58 +0100

    Began Implementing a AddNewTask java class and xml.

---
+++ app/src/main/java/com/example/actionplannerapplication/AddNewTask.java
@@ -0,0 +1,63 @@
+package com.example.actionplannerapplication;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class AddNewTask extends BottomSheetDialogFragment {
+
+    public static final String TAG = "AddNewTask";
+
+    private TextView setDueDate;
+    private EditText mTaskEdit;
+    private Button mSaveBtn;
+    private FirebaseFirestore firestore;
+    public static AddNewTask newInstance(){
+        return new AddNewTask();
+    }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.add_new_task, container, false);
+
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setDueDate = view.findViewById(R.id.set_due_tv);
+        mTaskEdit = view.findViewById(R.id.task_edittext);
+        mSaveBtn = view.findViewById(R.id.save_btn);
+
+        firestore = FirebaseFirestore.getInstance();
+
+        mTaskEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/actionplannerapplication/MainActivity.java
+++ app/src/main/java/com/example/actionplannerapplication/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.actionplannerapplication;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FloatingActionButton mFaButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        mFaButton = findViewById(R.id.floatingActionButton);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+        mFaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
